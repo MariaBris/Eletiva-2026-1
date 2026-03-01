35d7700ef6512f99af4dff1fdc03444707a02547
MariaBris <nanda.sanches.bris@gmail.com>
2026-03-01 12:53:05 -0300

    exercicio17 feito

---
+++ Aula-24-02/exercicio17.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercício 17</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercício 17</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="capital" class="form-label">Insira o valor do capital R$:</label>
+              <input type="text" id="capital" name="capital" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="juros" class="form-label">Insira a taxa de juros por mês(%):</label>
+              <input type="text" id="juros" name="juros" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="periodo" class="form-label">Insira o periodo(meses):</label>
+              <input type="text" id="periodo" name="periodo" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $capital = $_POST['capital'];
+        $juros = $_POST['juros'];
+        $periodo = $_POST['periodo'];
+        $rendimento = $capital * ($juros / 100) * $periodo;
+        $valorf = $rendimento + $capital;
+        echo "Valor do rendimento é de R$$rendimento, com valor final de R$$valorf";
+    }
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
