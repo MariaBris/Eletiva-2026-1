8751c6ff4e678eb762a11343cef3836d2c0dbce5
MariaBris <nanda.sanches.bris@gmail.com>
2026-03-01 09:56:30 -0300

    exercicio 6 feito

---
+++ Aula-24-02/exercicio6.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercício 6</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercício 6</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="celsius" class="form-label">Digite a temperatura em Celsius:</label>
+              <input type="number" id="celsius" name="celsius" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php 
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $celsius = $_POST['celsius'];
+        $tempf = ($celsius * (9/5)) + 32;
+        echo "A temperatura em Fahrenheit é: $tempf";
+    }
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
