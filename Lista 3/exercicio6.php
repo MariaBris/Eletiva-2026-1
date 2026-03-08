26b6b4a5f39feda6d46953a91f1711daa3018094
MariaBris <nanda.sanches.bris@gmail.com>
2026-03-07 21:43:22 -0300

    atualização do codigo

--- Lista 3/exercicio6.php
+++ Lista 3/exercicio6.php
@@ -20,8 +20,11 @@
     if($_SERVER['REQUEST_METHOD'] == "POST"){
         $num = $_POST['num'];
         for($i = 1; $i <= $num; $i++){
-            echo "$i\t-\t";
-        }
+            if($i == $num)
+                echo "$i.";
+            else
+                echo "$i\t-\t";
+            }
     }
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
