ad1213d82b03705d505b4fa1ac6ad2d486a5908e
MariaBris <nanda.sanches.bris@gmail.com>
2026-03-14 22:31:47 -0300

    exercicio13 feito

---
+++ Lista 4/exercicio13.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercício 13</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercício 13</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="frase" class="form-label">Insira uma frase:</label>
+              <input type="text" id="frase" name="frase" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $frase = $_POST['frase'];
+        $palavra = explode(" ",$frase);
+        $maior = $palavra[0];
+        for($i = 1; $i < str_word_count($frase); $i++){
+            if(strlen($palavra[$i]) > strlen($maior)){
+                $maior = $palavra[$i];
+            }
+        }
+        echo "<p>Número total de palavras na frase: ". str_word_count($frase) . "</p>";
+        echo "<p>A maior palavra da frase é : $maior</p>";
+    }
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
