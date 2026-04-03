c14e3203f7766e16d6946ec7157a849bfb092656
MariaBris <nanda.sanches.bris@gmail.com>
2026-04-03 16:42:33 -0300

    exercicio5 feito

---
+++ Lista 5/exercicio5.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercício 5</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercício 5</h1>
+<form method="post">
+    <?php
+    for($i = 0; $i < 3; $i++)
+        echo '<div class="mb-3">
+              <label for="nome[]" class="form-label">Insira o título do livro:</label>
+              <input type="text" id="nome[]" name="nome[]" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="qtde[]" class="form-label">Insira a quantidade em estoque:</label>
+              <input type="text" id="qtde[]" name="qtde[]" class="form-control" required="">
+            </div>'
+    ?>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $livros = [];
+        for($i = 0;$i < 3;$i++){
+            $nome = $_POST['nome'][$i];
+            $qtde = $_POST['qtde'][$i];
+            $livros[$nome] = $qtde;
+        } 
+        ksort($livros);
+        foreach($livros as $nome => $qtde){
+            if($qtde < 5)
+                echo "<p>Livro:  $nome | Estoque: $qtde | ***Atenção*** Estoque inferior a 5!</p>";
+            else
+                echo "<p>Livro:  $nome | Estoque: $qtde</p>";
+        }
+        }
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
