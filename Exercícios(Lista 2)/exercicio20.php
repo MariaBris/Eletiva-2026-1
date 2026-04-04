33e8fde43856898ca292eeface6578204986e4bf
MariaBris <nanda.sanches.bris@gmail.com>
2026-04-03 22:11:36 -0300

    atualização do echo

--- Exercícios(Lista 2)/exercicio20.php
+++ Exercícios(Lista 2)/exercicio20.php
@@ -24,7 +24,7 @@
         $distancia = $_POST['distancia'];
         $tempo = $_POST['tempo'];
         $resultado = $distancia / $tempo;
-        echo "Velocidade média de $resultado";
+        echo "Velocidade média de $resultado km/h";
     }
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
