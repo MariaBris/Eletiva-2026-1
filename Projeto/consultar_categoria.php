ead62004920dad0816596df6bf1684a56b50eeee
MariaBris <nanda.sanches.bris@gmail.com>
2026-05-05 21:15:25 -0300

    projetoprof

--- Projeto/consultar_categoria.php
+++ Projeto/consultar_categoria.php
@@ -1,5 +1,11 @@
 <?php
     require_once('cabecalho.php');
+    require_once('conexao.php');
+    try{
+        $stmt = $pdo->prepare('SELECT * FROM categoria WHERE id=?');
+    }catch(Exception $e){
+        echo "Erro!".$e->getMessage();
+    }
 ?>
 
 <h1>Consultar Categoria</h1>
